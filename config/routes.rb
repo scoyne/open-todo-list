0dcbe27c3cf543320f09b3badcc15f76db43dc81
scoyne <scoyne@bw.edu>
2018-07-31 19:03:04 -0400

    us2-authentication test successfull

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  User
-  List
-  Item
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
+
+  resources :lists
+  resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
